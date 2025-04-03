b796f6efce9303f0b2a91c56b549a5eec681327f
David <davidhodel6@gmail.com>
2025-04-03 20:30:10 +0200

    add static color examples

--- src/Common/Color.cs
+++ src/Common/Color.cs
@@ -3,13 +3,25 @@ using System.Numerics;
 
 namespace Common
 {
-    public readonly struct Color(float r, float g, float b)
+    public struct Color(float r, float g, float b)
     {
-        public readonly float R => r;
+        public static readonly Color Black = new(0, 0, 0);
+        public static readonly Color White = new(1, 1, 1);
+        public static readonly Color Red = new(1, 0, 0);
+        public static readonly Color Green = new(0, 1, 0);
+        public static readonly Color Blue = new(0, 0, 1);
+        public static readonly Color Yellow = new(1, 1, 0);
+        public static readonly Color Cyan = new(0, 1, 1);
+        public static readonly Color Magenta = new(1, 0, 1);
+        public static readonly Color Gray = new(0.5f, 0.5f, 0.5f);
+        public static readonly Color LightGray = new(0.75f, 0.75f, 0.75f);
+        public static readonly Color DarkGray = new(0.25f, 0.25f, 0.25f);
 
-        public readonly float G => g;
+        public float R { get; set; } = r;
 
-        public readonly float B => b;
+        public float G { get; set; } = g;
+
+        public float B { get; set; } = b;
 
         public static Color operator +(Color a, Color b) => new(a.R + b.R, a.G + b.G, a.B + b.B);
 
@@ -22,5 +34,40 @@ namespace Common
         public static Color operator *(Color color, int scalar) => new(color.R * scalar, color.G * scalar, color.B * scalar);
 
         public static Color operator *(Color color1, Color color2) => new(color1.R * color2.R, color1.G * color2.G, color1.B * color2.B);
+
+        public static bool operator ==(Color left, Color right) => left.R == right.R && left.G == right.G && left.B == right.B;
+
+        public static bool operator !=(Color left, Color right) => !(left == right);
+
+        /// <summary>
+        /// Performs linear interpolation between two colors.
+        /// </summary>
+        /// <param name="color1">The first color.</param>
+        /// <param name="color2">The second color.</param>
+        /// <param name="amount">The amount to interpolate (0.0 to 1.0).</param>
+        /// <returns>The interpolated color.</returns>
+        public static Color Lerp(Color color1, Color color2, float amount)
+        {
+            // Clamp amount to range [0, 1]
+            amount = Math.Clamp(amount, 0f, 1f);
+
+            return new Color(
+                color1.R + ((color2.R - color1.R) * amount),
+                color1.G + ((color2.G - color1.G) * amount),
+                color1.B + ((color2.B - color1.B) * amount)
+            );
+        }
+
+
+        /// <summary>
+        /// Calculates the brightness of the color using the formula:
+        /// 0.299 * R + 0.587 * G + 0.114 * B
+        /// </summary>
+        /// <returns></returns>
+        public float GetBrightness()
+        {
+            // Human perception weights: R=0.299, G=0.587, B=0.114
+            return (0.299f * R) + (0.587f * G) + (0.114f * B);
+        }
     }
 }
